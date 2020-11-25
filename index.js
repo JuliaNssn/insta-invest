37f2888ee7ccf7d079678a9d103f73e50cc3c846
Kounex <r.schramowski@googlemail.com>
2020-11-25 17:10:21 +0100

    no sandbox

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ function sendMail(investEntity) {
 
 async function checkAvailability(investEntity) {
   if (!investEntity.lastTimeAvailable || Date.now() - investEntity.lastTimeAvailable > 43200000) {
-    const browser = await puppy.launch();
+    const browser = await puppy.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
     const page = await browser.newPage();
 
     await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
