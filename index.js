526145e280079d150fda0f128c7ae34aaa747e6c
Kounex <r.schramowski@googlemail.com>
2020-11-26 11:34:39 +0100

    update launch

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ async function sendMail(investEntity) {
 
 async function checkAvailability(investEntity) {
   if (!investEntity.lastTimeAvailable || Date.now() - investEntity.lastTimeAvailable > 43200000) {
-    const browser = await puppy.launch({ slowMo: 500, args: ['--no-sandbox'] });
+    const browser = await puppy.launch({ args: ['--no-sandbox'] });
     const page = await browser.newPage();
 
     await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
