42e6b03a43a0d1545de1069f8973ed03bd0a3109
Kounex <r.schramowski@googlemail.com>
2020-11-26 11:07:57 +0100

    added cookies and headless

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const puppy = require('puppeteer');
 const nodemailer = require('nodemailer');
 const moment = require('moment'); // require
+const { json } = require('body-parser');
 
 require('dotenv').config();
 
@@ -21,7 +22,7 @@ const investies = [
     'PS 5',
     'https://www.amazon.de/Sony-Interactive-Entertainment-PlayStation-5/dp/B08H93ZRK9/',
     ['r.schramowski@googlemail.com', 'j.nissen@outlook.de', 'n.lipinski@web.de', 'p.schulz@eprivacy.eu'],
-    'nicht',
+    'unavailable',
     null
   ),
   new InvestEntity(
@@ -34,7 +35,7 @@ const investies = [
   ),
 ];
 
-function sendMail(investEntity) {
+async function sendMail(investEntity) {
   let transporter = nodemailer.createTransport({
     host: 'securesmtp.t-online.de',
     port: 465,
@@ -45,7 +46,7 @@ function sendMail(investEntity) {
     },
   });
 
-  transporter.sendMail({
+  await transporter.sendMail({
     from: `"Insta-Invest Mailer" <${process.env.FROMMAILUSER}>`,
     to: investEntity.emails.join(','),
     subject: investEntity.name + ' available!',
@@ -56,20 +57,21 @@ function sendMail(investEntity) {
 
 async function checkAvailability(investEntity) {
   if (!investEntity.lastTimeAvailable || Date.now() - investEntity.lastTimeAvailable > 43200000) {
-    const browser = await puppy.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+    const browser = await puppy.launch({ args: ['--no-sandbox'] });
     const page = await browser.newPage();
 
     await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
-
-    await page.goto(investEntity.link);
+    await page.setCookie(...JSON.parse(process.env.COOKIESFORPAGES));
 
     var availability;
 
     switch (investEntity.id) {
       case 0:
+        await page.goto(investEntity.link, { waitUntil: 'networkidle2' });
         availability = await page.$eval('div#availability .a-size-medium', (el) => el.innerHTML.trim());
         break;
       case 1:
+        await page.goto(investEntity.link, { waitUntil: 'networkidle2' });
         const divContainer = await page.$('[data-test="mms-delivery-online-availability"]');
         availability = await divContainer.$eval('div', (el) => el.innerHTML.trim().split('>')[1].split('<')[0]);
         break;
@@ -81,13 +83,11 @@ async function checkAvailability(investEntity) {
       investEntity.lastTimeAvailable = Date.now();
 
       console.log('[' + moment().format('DD.MM.YYY HH:mm:ss') + ']:', investEntity.name, 'GOGOGOGOGO');
-      sendMail(investEntity);
+      // await sendMail(investEntity);
     }
-
     await browser.close();
+    setTimeout(() => checkAvailability(investEntity), 5000);
   }
-
-  setTimeout(() => checkAvailability(investEntity), 5000);
 }
 
 investies.forEach((investEntity) => checkAvailability(investEntity));
